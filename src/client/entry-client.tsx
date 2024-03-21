eb4d5946d583c89397d84de86105593cf8534af6
Alex Golubev <golubev@email.com>
2024-03-21 09:45:33 +0200

    Implement basic client-side entry point in React
    Added a new file entry-client.tsx that includes the setup for client-side rendering using React. In this file, an application component with React.StrictMode and BrowserRouter is defined and rendered using either the createRoot or hydrateRoot method from 'react-dom/client', based on the condition of the import.meta['hot'] object or if the container does not yet contain any text content.

---
+++ src/client/entry-client.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
+import { createRoot, hydrateRoot } from 'react-dom/client';
+
+const container = document.getElementById('app') as HTMLElement;
+
+const Application = () => (
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+if (import.meta['hot'] || !container?.innerText) {
+  const root = createRoot(container);
+  root.render(<Application />);
+} else {
+  hydrateRoot(container, <Application />);
+}
